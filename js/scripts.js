3684a3bbcfdaf127d91612f588b141930976c840
Parnak Ruslan <radvancep2@gmail.com>
2022-11-05 18:28:58 +0300

    add burger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
+// function test() {
+//   fetch("./youtJSON.json")
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+//   return data;
+// }
+
+// let info = test();
+
 window.scrollBy(0, 1);
 
 let pageUp = document.querySelector("#pageUp");
 
 document.onscroll = function () {
-  if (window.scrollY < 100) {
+  if (window.scrollY < 50) {
     pageUp.classList.add("page-up-hidden");
   } else {
     pageUp.classList.remove("page-up-hidden");
   }
 };
+
+let burger = document.querySelector(".burger"),
+  menu = document.querySelector(".menu");
+
+burger.addEventListener("click", function () {
+  burger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
