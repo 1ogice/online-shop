c6edc5d81b06d038ac2103a43ca281d87fb4119a
Parnak Ruslan <radvancep2@gmail.com>
2022-11-14 20:00:35 +0300

    fix js files

--- js/products.js
+++ js/products.js
@@ -1,28 +1,34 @@
 import products from "./products.json" assert { type: "json" };
 
-let burger = document.querySelector(".burger"),
-  menu = document.querySelector(".menu");
+(function burgerSwitch() {
+  let burger = document.querySelector(".burger"),
+    menu = document.querySelector(".menu");
 
-burger.addEventListener("click", function () {
-  burger.classList.toggle("active");
-  menu.classList.toggle("active");
-});
-
-let cart = document.querySelector(".cart"),
-  basket = document.querySelector(".icon-basket"),
-  close = document.querySelector(".cart-close");
-
-basket.addEventListener("click", function () {
-  cart.classList.toggle("active");
-});
-close.addEventListener("click", function () {
-  cart.classList.remove("active");
-});
-
-const accordion = document.getElementsByClassName("container");
-
-for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function () {
-    this.classList.toggle("active");
+  burger.addEventListener("click", function () {
+    burger.classList.toggle("active");
+    menu.classList.toggle("active");
   });
-}
+})();
+
+(function cartSwitch() {
+  let cart = document.querySelector(".cart"),
+    basket = document.querySelector(".icon-basket"),
+    close = document.querySelector(".cart-close");
+
+  basket.addEventListener("click", function () {
+    cart.classList.toggle("active");
+  });
+  close.addEventListener("click", function () {
+    cart.classList.remove("active");
+  });
+})();
+
+(function accordionSwitch() {
+  const accordion = document.getElementsByClassName("container");
+
+  for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+    });
+  }
+})();
